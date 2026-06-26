1e2c2b7402008da1d4c5067e532cec100ae6c70f
Tiago Mulizine <tiagomulizine211991@gmail.com>
2026-06-26 15:57:28 -0300

    ajuste porta render

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ app.post('/validar', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(Servidor rodando na porta ${PORT});
 });
